ba0dc3cb4b29e006d27b1aa770917066de050742
Khan Thompson <khanage@gmail.com>
2014-08-03 17:46:05 +1000

    Add in maybe tryparse wrappers

---
+++ Bearded.Monads/MaybeParseExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Bearded.Monads
+{
+    public static class MaybeParseExtensions
+    {
+        public static Option<int> MaybeInt(this string value)
+        {
+            int val;
+
+            if (int.TryParse(value, out val))
+                return val;
+
+            return Option<int>.None;
+        }
+
+        public static Option<float> MaybeFloat(this string value)
+        {
+            int val;
+
+            if (int.TryParse(value, out val))
+                return val;
+
+            return Option<float>.None;
+        }
+
+        public static Option<A> MaybeEnum<A>(this string value, bool ignoreCase = false)
+            where A : struct
+        {
+            A val;
+
+            if (Enum.TryParse(value, ignoreCase, out val))
+                return val;
+
+            return Option<A>.None;
+        }
+    }
+}

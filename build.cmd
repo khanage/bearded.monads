c3a316ad688ab91673b7eceec508aa7ace58e5f4
Khan Thompson <khan.thompson@carsales.com.au>
2016-08-19 15:31:32 +1000

    Play with path for appveyor

--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-packages\Leeloo\tools\Fake.exe leeloo\build.fsx
+.\packages\Leeloo\tools\Fake.exe leeloo\build.fsx
 
 REM This will leave the console opened if it was opened from windows
 for %%x in (%cmdcmdline%) do if /i "%%~x"=="/c" set DOUBLECLICKED=1

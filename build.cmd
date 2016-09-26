87dc31643a8d55665c9cceb20b0842b861457ab1
Khan Thompson <khan.thompson@carsales.com.au>
2016-09-26 10:37:31 +1000

    Remove pause in build script - seems unhappy in appveyor

--- build.cmd
+++ build.cmd
@@ -3,5 +3,5 @@
 .\packages\Leeloo\tools\Fake.exe leeloo\build.fsx
 
 REM This will leave the console opened if it was opened from windows
-for %%x in (%cmdcmdline%) do if /i "%%~x"=="/c" set DOUBLECLICKED=1
-if defined DOUBLECLICKED pause
+REM for %%x in (%cmdcmdline%) do if /i "%%~x"=="/c" set DOUBLECLICKED=1
+REM if defined DOUBLECLICKED pause

d00b9b2e9601692c3ea05fefc772bda137f033a8
khanage <khanage@gmail.com>
2018-12-21 09:48:15 +1100

    Add Alternative for Either

--- Bearded.Monads/Alternative.cs
+++ Bearded.Monads/Alternative.cs
@@ -11,5 +11,13 @@ namespace Bearded.Monads
         public static Either<Success, Error> Alternatively<Success, Error>
             (this Either<Success, Error> initial, Either<Success, Error> subsequent) =>
             initial.Alternatively(() => subsequent);
+        
+        public static Try<Success> Alternatively<Success>
+            (this Try<Success> initial, Func<Try<Success>> subsequent) =>
+            initial.Unify(Try<Success>.Create, _ => subsequent());
+
+        public static Try<Success> Alternatively<Success>
+            (this Try<Success> initial, Try<Success> subsequent) =>
+            initial.Alternatively(() => subsequent);
     }
 }
